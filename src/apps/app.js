a0df2b7da7ecc861c94a4c709ef7cbe4058ca272
Oleh8978 <mihajlovskyoleg@gmail.com>
2020-11-11 19:34:35 +0200

    The final version of the code after in total 4 hours work

--- src/apps/app.js
+++ src/apps/app.js
@@ -4,6 +4,10 @@ const checker = express.Router();
 checker.use(express.json());
 
 const domain = require("./domainZones/domains");
+const checkInputAndLookUp = require("./function/functions");
+const idGenerate = require("./idGenerator/idgenrqator");
+const validateEntry = require("./validators/validators");
+const checkIfValuIsValidForFunction = require("./validators/checkers");
 
 checker.use(
   express.urlencoded({
@@ -11,74 +15,54 @@ checker.use(
   })
 );
 
-let checkInputAndLookUp = (name, surname, company, domains) => {
-  let wordsF = [];
-  let wordsL = [];
-  let one = (name, nameLength, arr) => {
-    for (let i = 0; i < nameLength; i++) {
-      arr.push(name[i]);
-    }
-    for (let j = 0; j < nameLength; j++) {
-      if (j < nameLength - 1) {
-        arr.push(name[j] + name[j + 1]);
-      }
-    }
-    for (let l = 0; l < nameLength; l++) {
-      if (l < nameLength - 2) {
-        arr.push(name[l] + name[l + 1] + name[l + 2]);
-      }
-    }
-
-    return arr;
-  };
-  // function for the last name
-
-  let two = (surname, surnameLength, arr) => {
-    for (let q = 0; q < surnameLength; q++) {
-      arr.push(surname[q]);
-    }
-    return arr;
-  };
-  let firstSection = one(name, name.length, wordsF);
-  let secondSection = two(surname, surname.length, wordsL);
-  // function to check the bist one from 2 arrays
-  const checkTheBigestOne = (first, second) => {
-    let Bigest = [];
-    if (first.length !== 0 && second.length !== 0) {
-      if (first.length > second.length) {
-        return (Bigest = first);
-      } else if (second.length > first.length) {
-        return (Bigest = second);
-      } else if (first.length === 0) {
-        return (Bigest = second);
-      } else if (second.length === 0) {
-        Bigest = first;
-      } else if ((first.length === second.length) === 0) {
-        return (Bigest = []);
-      }
-    }
-    return Bigest;
-  };
-  let value = checkTheBigestOne(firstSection, secondSection);
-
-  let finalArrBeforeDNSCheck = () => {
-    let arr;
-    return arr;
-  };
-
-  return value;
-};
-
 checker.post("/", (req, res, next) => {
-  let name = req.body.name;
-  let surname = req.body.surname;
-  let company = req.body.company;
-  console.log(checkInputAndLookUp(name, surname, company, domain));
-  res.status(201).json({
-    name: name + "",
-    surname: surname + "",
-    company: company + ""
-  });
+  let name = validateEntry(req.body.name);
+  let surname = validateEntry(req.body.surname);
+  let company = validateEntry(req.body.company);
+  let arrOfAvailableEmails = [];
+  let arrOfObjectsForJson = [];
+  if (
+    checkIfValuIsValidForFunction(
+      req.body.name,
+      name,
+      req.body.surname,
+      surname,
+      req.body.company,
+      company
+    )
+  ) {
+    arrOfAvailableEmails = checkInputAndLookUp(name, surname, company, domain);
+    for (let i = 0; i < arrOfAvailableEmails.length; i++) {
+      arrOfObjectsForJson.push({
+        id: idGenerate(),
+        mail: arrOfAvailableEmails[i]
+      });
+    }
+  }
+  // console.log(arrOfObjectsForJson);
+  // console.log(checkInputAndLookUp(name, surname, company, domain));
+  // console.log(idGenerate());
+
+  if (arrOfObjectsForJson.length !== 0) {
+    res.status(201).json({
+      number: arrOfObjectsForJson.length,
+      mails: arrOfObjectsForJson.map((mail) => {
+        //  console.log(mail.mail);
+        return {
+          id: mail.id,
+          mail: mail.mail
+        };
+      })
+    });
+  } else {
+    res.status(201).json({
+      name: name,
+      surname: surname,
+      company: company,
+      err:
+        "Warning! Validate entry! Note: do not leave empty spaces and do not put invalid charcters, all fields should be filled up"
+    });
+  }
 });
 
 module.exports = checker;
